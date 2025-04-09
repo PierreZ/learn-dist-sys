a703574b20ae2773d3101c63e42c100dcc1ade3f
Pierre Zemb <pierre.zemb@clever-cloud.com>
2025-04-09 16:01:57 +0200

    fix flake

--- flake.nix
+++ flake.nix
@@ -5,24 +5,32 @@
 
   outputs = { self, nixpkgs }:
     let
-      javaVersion = 22; # Change this value to update the whole stack
+      javaVersion = 23; # Change this value to update the whole stack
 
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      # Define the overlay
+      javaOverlay = final: prev: rec {
+        jdk = prev."jdk${toString javaVersion}";
+        maven = prev.maven.override { jre = jdk; };
+        gradle = prev.gradle.override { java = jdk; };
+      };
+
+      # Use the overlay when importing nixpkgs
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; overlays = [ javaOverlay ]; };
       });
     in
     {
-      overlays.default =
-        final: prev: rec {
-          jdk = prev."jdk${toString javaVersion}";
-          maven = prev.maven.override { jre = jdk; };
-          gradle = prev.gradle.override { java = jdk; };
-        };
+      # Export the overlay
+      overlays.default = javaOverlay;
 
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [ jdk gradle graphviz gnuplot maelstrom-clj jbang ];
+          shellHook = ''
+            export JAVA_HOME=${pkgs.jdk}/lib/openjdk
+          '';
         };
       });
     };
