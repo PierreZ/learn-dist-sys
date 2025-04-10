7b6278212e09be401e5293aa7369453027e5d0ca
Pierre Zemb <pierre.zemb@clever-cloud.com>
2025-04-10 10:03:56 +0200

    reset step-3

--- step-3/run.sh
+++ step-3/run.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
+set -ex
 
-../bin/maelstrom test -w broadcast --bin ./Broadcast.java --time-limit 10 --rate 10 --node-count 3
+# Java file to run - change this to test different implementations
+JAVA_FILE="Broadcast.java"
+
+# Make sure the file is executable
+chmod +x "$JAVA_FILE"
+
+# Build the file first to ensure it compiles
+jbang build "$JAVA_FILE"
+
+# Run the test with Maelstrom
+../bin/maelstrom test -w broadcast --bin "./$JAVA_FILE" --node-count 5 --time-limit 20 --rate 10
