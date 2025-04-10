2786ed95862c066c229eab6bb1c4bf5f90d36abb
Pierre Zemb <pierre.zemb@clever-cloud.com>
2025-04-10 08:25:42 +0200

    bootstrap step-3

---
+++ step-3/VirtualThreadDemo.java
@@ -0,0 +1,24 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+public class VirtualThreadDemo {
+
+  public static void main(String... args) throws InterruptedException {
+    Thread.ofVirtual().start(() -> {
+
+      System.out.println("toto");
+      System.out.println(Thread.currentThread());
+    });
+
+    // Platform (or OS) thread
+    Thread.ofPlatform().start(() -> {
+      System.out.println(Thread.currentThread());
+    });
+
+    // Virtual thread
+    Thread.ofVirtual().start(() -> {
+      System.out.println(Thread.currentThread());
+    });
+
+    Thread.sleep(1000);
+  }
+}
---
+++ step-3/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+../bin/maelstrom test -w broadcast --bin ./Broadcast.java --time-limit 10 --rate 10 --node-count 3
